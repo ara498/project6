7b190920042efcd92142c969eb6b4509386f135d
ara498 <aravindsteeve11@gmail.com>
2024-08-20 10:46:54 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def test1(request):
+    return render(request,'test1.html')
+def test2(request):
+    return render(request,'test2.html')
+def test3(request):
+    return render(request,'test3.html')  
+def test4(request):
+    return render(request,'test4.html') 
+def test5(request):
+    return render(request,'test5.html')      
+            
